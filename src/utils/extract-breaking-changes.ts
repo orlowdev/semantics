9579518d9450c0e5e2299a832114ed79854b3bd1
Сергей Орлов <priestine1.dev@gmail.com>
2018-10-11 18:55:56 +1000

    fix(#1): Amend reference to breaking changes to follow CC

--- src/utils/extract-breaking-changes.ts
+++ src/utils/extract-breaking-changes.ts
@@ -1,7 +1,7 @@
 import { CommitInterface } from '../interfaces/commit.interface';
 
 export const extractBreakingChanges = (commit: CommitInterface): CommitInterface => {
-  commit.breakingChanges = !!commit.body.find((x: string) => /^BREAKING\sCHANGES:/.test(x));
+  commit.breakingChanges = !!commit.body.find((x: string) => /^BREAKING\sCHANGE:/.test(x));
 
   return commit;
 };
