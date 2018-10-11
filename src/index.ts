facc0d47e3c77f8a5bc57bba447ee52160b48ba5
Сергей Орлов <priestine1.dev@gmail.com>
2018-10-11 17:49:28 +1000

    style: Fix styling
    * Normalize indentation
    * Dummy content for checking lists in commit body

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,18 @@ execPromise('git rev-parse HEAD')
     execPromise('git rev-parse --abbrev-ref HEAD')
       .then((branch: string) => {
         execPromise('git describe --tags `git rev-list --tags --max-count=1`')
-        .then((currentTag: string) => {
-          execPromise(`git show-ref ${currentTag} -s`)
-          .then((latestTaggedCommit: string) => {
-            execPromise(`git rev-list ${latestTaggedCommit}..HEAD --no-merges --format='${commitFormat}'`)
-            .then((changes: string) => {
-              Shell.write(JSON.parse(normalizeChanges(changes)));
-            })
-            .catch(catchError);
+          .then((currentTag: string) => {
+            execPromise(`git show-ref ${currentTag} -s`)
+              .then((latestTaggedCommit: string) => {
+                execPromise(`git rev-list ${latestTaggedCommit}..HEAD --no-merges --format='${commitFormat}'`)
+                  .then((changes: string) => {
+                    Shell.write(JSON.parse(normalizeChanges(changes)));
+                  })
+                  .catch(catchError);
+              })
+              .catch(catchError);
           })
           .catch(catchError);
-        })
-        .catch(catchError);
       })
       .catch(catchError);
   })
