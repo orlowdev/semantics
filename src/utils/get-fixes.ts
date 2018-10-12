37bb70da10ae04eeea08a694196d139a1a91ff83
Сергей Орлов <priestine1.dev@gmail.com>
2018-10-12 11:25:57 +1000

    feat(#1): Add building fixes changelog

--- src/utils/get-fixes.ts
+++ src/utils/get-fixes.ts
@@ -1,3 +1,6 @@
 import { CommitInterface } from '../interfaces/commit.interface';
 
-export const getFixes = (xs: CommitInterface[]): string[] => [];
+export const getFixes = (xs: CommitInterface[]): string[] =>
+  xs
+    .filter((x: CommitInterface) => x.type === 'fix')
+    .map((x: CommitInterface) => `${x.abbrevHash} ${x.type}: ${x.subject}`);
