1c2e478c8ca0fafd3876b0cdecc9d3e271b5241e
Сергей Орлов <priestine1.dev@gmail.com>
2018-10-12 13:09:52 +1000

    perf(#1): Memorize current version for later use

--- src/utils/change-version.ts
+++ src/utils/change-version.ts
@@ -1,23 +1,25 @@
 import { getCurrentVersion } from './get-current-version';
 
-export const changeVersion = (currentTag: string) => (major: boolean, minor: boolean, patch: boolean): string => {
+export const changeVersion = (currentTag: string) => {
   const current = getCurrentVersion(currentTag);
 
-  if (!current[0]) {
-    return '1.0.0';
-  }
+  return (major: boolean, minor: boolean, patch: boolean): string => {
+    if (!current[0]) {
+      return '1.0.0';
+    }
 
-  if (major) {
-    return `${current[0] + 1}.0.0`;
-  }
+    if (major) {
+      return `${current[0] + 1}.0.0`;
+    }
 
-  if (minor) {
-    return `${current[0]}.${current[1] + 1}.0`;
-  }
+    if (minor) {
+      return `${current[0]}.${current[1] + 1}.0`;
+    }
 
-  if (patch) {
-    return `${current[0]}.${current[1]}.${current[1] + 1}`;
-  }
+    if (patch) {
+      return `${current[0]}.${current[1]}.${current[1] + 1}`;
+    }
 
-  return `${current[0]}.${current[1]}.${current[2]}`;
+    return `${current[0]}.${current[1]}.${current[2]}`;
+  };
 };
