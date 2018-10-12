aaad06c217658bfab5937796ee7b95d2835b1812
Сергей Орлов <priestine1.dev@gmail.com>
2018-10-12 11:49:51 +1000

    fix(#1): Fix rendering changelog

--- src/utils/build-changelog.ts
+++ src/utils/build-changelog.ts
@@ -13,53 +13,61 @@ export const buildChangelog = (
   cis: string[],
   breakingChanges: string[]
 ): string => `# ${version}
-
-${features.length ? `## ${CommitTypes.feat.title}
+${features.length ? `
+## ${CommitTypes.feat.title}
 
 > ${CommitTypes.feat.description}
 
-${features.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${fixes.length ? `## ${CommitTypes.fix.title}
+${features.map((f: string) => `* ${f}`).join('\n')}` : ''}${fixes.length ? `
+
+## ${CommitTypes.fix.title}
 
 > ${CommitTypes.fix.description}
 
-${fixes.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${refactors.length ? `## ${CommitTypes.refactor.title}
+${fixes.map((f: string) => `* ${f}`).join('\n')}` : ''}${refactors.length ? `
+
+## ${CommitTypes.refactor.title}
 
 > ${CommitTypes.refactor.description}
 
-${refactors.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${perfs.length ? `## ${CommitTypes.perf.title}
+${refactors.map((f: string) => `* ${f}`).join('\n')}` : ''}${perfs.length ? `
+
+## ${CommitTypes.perf.title}
 
 > ${CommitTypes.perf.description}
 
-${perfs.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${tests.length ? `## ${CommitTypes.test.title}
+${perfs.map((f: string) => `* ${f}`).join('\n')}` : ''}${tests.length ? `
+## ${CommitTypes.test.title}
 
 > ${CommitTypes.test.description}
 
-${tests.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${builds.length ? `## ${CommitTypes.build.title}
+${tests.map((f: string) => `* ${f}`).join('\n')}` : ''}${builds.length ? `
+
+## ${CommitTypes.build.title}
 
 > ${CommitTypes.build.description}
 
-${builds.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${cis.length ? `## ${CommitTypes.ci.title}
+${builds.map((f: string) => `* ${f}`).join('\n')}` : ''}${cis.length ? `
+
+## ${CommitTypes.ci.title}
 
 > ${CommitTypes.ci.description}
 
-${cis.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${chores.length ? `## ${CommitTypes.chore.title}
+${cis.map((f: string) => `* ${f}`).join('\n')}` : ''}${chores.length ? `
+
+## ${CommitTypes.chore.title}
 
 > ${CommitTypes.chore.description}
 
-${chores.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${reverts.length ? `## ${CommitTypes.revert.title}
+${chores.map((f: string) => `* ${f}`).join('\n')}` : ''}${reverts.length ? `
+
+## ${CommitTypes.revert.title}
 
 > ${CommitTypes.revert.description}
 
-${reverts.map((f: string) => `* ${f}`).join('\n')}` : ''}
-${breakingChanges.length ? `## BREAKING CHANGES
+${reverts.map((f: string) => `* ${f}`).join('\n')}` : ''}${breakingChanges.length ? `
+
+## BREAKING CHANGES
 
 > Something backwards-incompatible
 
