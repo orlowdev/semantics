3696e40792a1a2d84973c3b0cef27f828d1c2fcd
Сергей Орлов <priestine1.dev@gmail.com>
2018-10-12 11:26:11 +1000

    feat(#1): Add building reverts changelog

--- src/utils/get-reverts.ts
+++ src/utils/get-reverts.ts
@@ -1,3 +1,6 @@
 import { CommitInterface } from '../interfaces/commit.interface';
 
-export const getReverts = (xs: CommitInterface[]): string[] => [];
+export const getReverts = (xs: CommitInterface[]): string[] =>
+  xs
+    .filter((x: CommitInterface) => x.type === 'revert')
+    .map((x: CommitInterface) => `${x.abbrevHash} ${x.type}: ${x.subject}`);
