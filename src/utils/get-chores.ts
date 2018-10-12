24a3e6478648095ec0a495f8fd9fbca56c8dd361
Сергей Орлов <priestine1.dev@gmail.com>
2018-10-12 11:25:29 +1000

    feat(#1): Add building features changelog

--- src/utils/get-chores.ts
+++ src/utils/get-chores.ts
@@ -1,3 +1,6 @@
 import { CommitInterface } from '../interfaces/commit.interface';
 
-export const getChores = (xs: CommitInterface[]): string[] => [];
+export const getChores = (xs: CommitInterface[]): string[] =>
+  xs
+    .filter((x: CommitInterface) => ['chore', 'docs', 'style', 'refactor', 'test', 'build', 'ci'].includes(x.type))
+    .map((x: CommitInterface) => `${x.abbrevHash} ${x.type}: ${x.subject}`);
