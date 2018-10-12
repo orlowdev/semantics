c69eaeeed1d8812424a03d71e99b61d0b0042420
Сергей Орлов <priestine1.dev@gmail.com>
2018-10-12 11:25:45 +1000

    feat(#1): Add building features changelog

--- src/utils/get-features.ts
+++ src/utils/get-features.ts
@@ -1,3 +1,6 @@
 import { CommitInterface } from '../interfaces/commit.interface';
 
-export const getFeatures = (xs: CommitInterface[]): string[] => [];
+export const getFeatures = (xs: CommitInterface[]): string[] =>
+  xs
+    .filter((x: CommitInterface) => x.type === 'feat')
+    .map((x: CommitInterface) => `${x.abbrevHash} ${x.type}: ${x.subject}`);
