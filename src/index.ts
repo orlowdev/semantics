92561478240cce92cbe495021acafa2b2908a1e3
priestine1 <priestine1.dev@gmail.com>
2018-10-13 21:52:35 +1000

    fix(#1): Remove checking current branch

--- src/index.ts
+++ src/index.ts
@@ -18,86 +18,77 @@ import { flatten } from './utils/flatten-array';
 
 execPromise('git rev-parse HEAD')
   .then((currentCommit: string) => {
-    execPromise('git rev-parse --abbrev-ref HEAD')
-      .then((branch: string) => {
-        if (branch !== 'master') {
-          Shell.info('Version bumping being executed in branch other than master.');
-          return;
-        }
+    execPromise('git describe --tags `git rev-list --tags --max-count=1`')
+      .then((currentTag: string) => {
+        const versionChanger = changeVersion(currentTag);
 
-        execPromise('git describe --tags `git rev-list --tags --max-count=1`')
-          .then((currentTag: string) => {
-            const versionChanger = changeVersion(currentTag);
+        execPromise(`git show-ref ${currentTag} -s`)
+          .then((latestTaggedCommit: string) => {
+            execPromise(`git rev-list ${latestTaggedCommit}..HEAD --no-merges --format='${commitFormat}'`)
+              .then((changes: string) => {
+                const normalizedChanges: CommitInterface[] = JSON.parse(normalizeChanges(changes))
+                  .map(normalizeBody)
+                  .map(extractCommitTypes)
+                  .map(extractBreakingChanges);
 
-            execPromise(`git show-ref ${currentTag} -s`)
-              .then((latestTaggedCommit: string) => {
-                execPromise(`git rev-list ${latestTaggedCommit}..HEAD --no-merges --format='${commitFormat}'`)
-                  .then((changes: string) => {
-                    const normalizedChanges: CommitInterface[] = JSON.parse(normalizeChanges(changes))
-                      .map(normalizeBody)
-                      .map(extractCommitTypes)
-                      .map(extractBreakingChanges);
+                const getCommitSubjects = getSubjects(normalizedChanges);
+                const amendMajor = getAmendMajor(normalizedChanges);
+                const amendMinor = getAmendMinor(normalizedChanges);
+                const amendPatch = getAmendPatch(normalizedChanges);
+                const newVersion = versionChanger(amendMajor, amendMinor, amendPatch);
 
-                    const getCommitSubjects = getSubjects(normalizedChanges);
-                    const amendMajor = getAmendMajor(normalizedChanges);
-                    const amendMinor = getAmendMinor(normalizedChanges);
-                    const amendPatch = getAmendPatch(normalizedChanges);
-                    const newVersion = versionChanger(amendMajor, amendMinor, amendPatch);
+                if (currentTag === `${newVersion[0]}.${newVersion[1]}.${newVersion[2]}`) {
+                  Shell.info('Given changes do not require releasing.');
+                  return;
+                }
 
-                    if (currentTag === `${newVersion[0]}.${newVersion[1]}.${newVersion[2]}`) {
-                      Shell.info('Given changes do not require releasing.');
-                      return;
-                    }
+                const features = getCommitSubjects('feat');
+                const fixes = getCommitSubjects('fix');
+                const chores = getCommitSubjects('chore');
+                const reverts = getCommitSubjects('revert');
+                const tests = getCommitSubjects('test');
+                const refactors = getCommitSubjects('refactor');
+                const perfs = getCommitSubjects('perf');
+                const builds = getCommitSubjects('build');
+                const docs = getCommitSubjects('docs');
+                const styles = getCommitSubjects('style');
+                const cis = getCommitSubjects('ci');
+                const breakingChanges = flatten(normalizedChanges.map((x: CommitInterface) => x.breakingChanges));
+                const changeLog = buildChangelog(
+                  newVersion,
+                  features,
+                  fixes,
+                  chores,
+                  reverts,
+                  tests,
+                  refactors,
+                  perfs,
+                  builds,
+                  cis,
+                  docs,
+                  styles,
+                  breakingChanges
+                );
 
-                    const features = getCommitSubjects('feat');
-                    const fixes = getCommitSubjects('fix');
-                    const chores = getCommitSubjects('chore');
-                    const reverts = getCommitSubjects('revert');
-                    const tests = getCommitSubjects('test');
-                    const refactors = getCommitSubjects('refactor');
-                    const perfs = getCommitSubjects('perf');
-                    const builds = getCommitSubjects('build');
-                    const docs = getCommitSubjects('docs');
-                    const styles = getCommitSubjects('style');
-                    const cis = getCommitSubjects('ci');
-                    const breakingChanges = flatten(normalizedChanges.map((x: CommitInterface) => x.breakingChanges));
-                    const changeLog = buildChangelog(
-                      newVersion,
-                      features,
-                      fixes,
-                      chores,
-                      reverts,
-                      tests,
-                      refactors,
-                      perfs,
-                      builds,
-                      cis,
-                      docs,
-                      styles,
-                      breakingChanges
-                    );
-
-                    request.post(
-                      `https://gitlab.com/api/v4/projects/${process.env.CI_PROJECT_ID}/repository/tags`,
-                      {
-                        headers: {
-                          'PRIVATE-TOKEN': process.env.PRIVATE_TOKEN,
-                        },
-                        json: true,
-                        body: {
-                          id: process.env.CI_PROJECT_ID,
-                          tag_name: newVersion,
-                          ref: currentCommit,
-                          release_description: changeLog,
-                        },
-                      },
-                      (e: Error, r, b) => {
-                        if (e) Shell.error(e.message);
-                        Shell.success(`Successfully released ${newVersion}`);
-                      }
-                    );
-                  })
-                  .catch(catchError);
+                request.post(
+                  `https://gitlab.com/api/v4/projects/${process.env.CI_PROJECT_ID}/repository/tags`,
+                  {
+                    headers: {
+                      'PRIVATE-TOKEN': process.env.PRIVATE_TOKEN,
+                    },
+                    json: true,
+                    body: {
+                      id: process.env.CI_PROJECT_ID,
+                      tag_name: newVersion,
+                      ref: currentCommit,
+                      release_description: changeLog,
+                    },
+                  },
+                  (e: Error, r, b) => {
+                    if (e) Shell.error(e.message);
+                    Shell.success(`Successfully released ${newVersion}`);
+                  }
+                );
               })
               .catch(catchError);
           })
