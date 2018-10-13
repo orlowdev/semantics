02cec500888159c65f667834cc55571a18afde7c
priestine1 <priestine1.dev@gmail.com>
2018-10-13 21:27:09 +1000

    fix(#1): Add trimming hyphens in commit bodies

--- src/utils/normalize-body.ts
+++ src/utils/normalize-body.ts
@@ -9,6 +9,9 @@ import { CommitInterface } from '../interfaces/commit.interface';
 export const normalizeBody = (commit: CommitInterface): CommitInterface => {
   commit.body = (commit.body as any)
     .split(', ')
-    .reduce((r: string[], x: string) => (x ? r.concat([/^\*\s+/.test(x) ? x.replace(/^\*\s+/, '') : x]) : r), []);
+    .reduce(
+      (r: string[], x: string) => (x ? r.concat([/^(\*|-)\s+/.test(x) ? x.replace(/^(\*|-)\s+/, '') : x]) : r),
+      []
+    );
   return commit;
 };
