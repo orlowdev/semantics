61e61e4ec2b25fff3ffa96fa544158159f36f780
priestine1 <priestine1.dev@gmail.com>
2018-10-15 18:20:00 +1000

    revert(#8): Remove building from Dockerfile on each release

--- build.sh
+++ build.sh
@@ -2,8 +2,4 @@ if [[ -f '.tmp.version_data' ]]; then
     sed -i s/"\"version\":.*/\"version\": \"$(cat .tmp.version_data)\","/ package.json;
     echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
     npm publish
-
-    docker build . -t priestine/semantics:$(cat .tmp.version_data)
-    docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-    docker push priestine/semantics
 fi
