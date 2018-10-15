d5221934d8d9e9e13e85e5d26b77d2bf65a2f46b
priestine1 <priestine1.dev@gmail.com>
2018-10-15 16:25:03 +1000

    fix(#14): Add exporting env var NEW_VERSION

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 if [[ -f '.tmp.version_data' ]]; then
-    sed -i s/\"version\":.*/\"version\": \"$(cat .tmp.version_data)\"/ package.json
+    sed -i s/"\"version\":.*/\"version\": \"$(cat .tmp.version_data)\","/ package.json;
+    echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
+    npm publish
 fi
