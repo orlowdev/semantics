36af8aaa38cea6613c773ce55390a09d7e5898d0
priestine1 <priestine1.dev@gmail.com>
2018-10-15 23:38:27 +1000

    fix(#14): Last attempt to fix publishing for today

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 if [[ -f '.tmp.version_data' ]]; then
     sed -i s/"\"version\":.*/\"version\": \"$(cat .tmp.version_data)\","/ package.json
-    tsc
+    yarn build:ci
+    ls -al
     echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
     npm publish
 fi
