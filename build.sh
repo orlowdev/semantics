54e03625ac6259c3d53487809b834b8933453f6d
priestine1 <priestine1.dev@gmail.com>
2018-10-15 23:25:43 +1000

    fix(#14): Add building before publishing

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 if [[ -f '.tmp.version_data' ]]; then
     sed -i s/"\"version\":.*/\"version\": \"$(cat .tmp.version_data)\","/ package.json
+    tsc
     echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
     npm publish
 fi
