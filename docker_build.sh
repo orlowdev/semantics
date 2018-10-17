40c5eda157bf2493566031697f58f0e0971cebf3
priestine1 <priestine1.dev@gmail.com>
2018-10-17 16:40:29 +1000

    ci(#30): Add building Dockerfiles on release

--- docker_build.sh
+++ docker_build.sh
@@ -1,10 +1,10 @@
 # Build Alpine Docker images
-docker build -t priestine/semantics:$(cat .tmp.version_data)-alpine - < ./docker/alpine.Dockerfile
+docker build -t priestine/semantics:$(git describe --tags `git rev-list --tags --max-count=1`)-alpine - < ./docker/alpine.Dockerfile
 docker build -t priestine/semantics:latest - < ./docker/alpine.Dockerfile
 docker build -t priestine/semantics:alpine - < ./docker/alpine.Dockerfile
 
 # Build Slim Docker images
-docker build -t priestine/semantics:$(cat .tmp.version_data)-slim - < ./docker/slim.Dockerfile
+docker build -t priestine/semantics:$(git describe --tags `git rev-list --tags --max-count=1`)-slim - < ./docker/slim.Dockerfile
 docker build -t priestine/semantics:slim - < ./docker/slim.Dockerfile
 
 # Push images to Docker Cloud
