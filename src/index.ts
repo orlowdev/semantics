4cf715b30a532ed0acda6d855eb016374c7170eb
Sergei Orlov <priestine1.dev@gmail.com>
2019-06-05 22:14:52 +0300

    chore: remove redundant tap-log helpers

--- src/index.ts
+++ src/index.ts
@@ -107,11 +107,6 @@ export abstract class Messenger {
   public static error(...message): typeof Iro {
     return Iro.log(Iro.red(Iro.inverse(' SEMANTICS ERROR   '), '  '), Iro.white(...message));
   }
-
-  public static tapInfo = (...message) => R.tap(() => Messenger.info(...message));
-  public static tapSuccess = (...message) => R.tap(() => Messenger.success(...message));
-  public static tapWarning = (...message) => R.tap(() => Messenger.warning(...message));
-  public static tapError = (...message) => R.tap(() => Messenger.error(...message));
 }
 
 export const getBreakingChanges = (changes: CommitInterface[]): string => {
