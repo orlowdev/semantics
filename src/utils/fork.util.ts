fccde2843cb62247ed218814619fe218500ecbef
Sergei Orlov <priestine1.dev@gmail.com>
2019-06-10 17:09:10 +0300

    chore: remove fork as R.ifElse is used instead

--- src/utils/fork.util.ts
+++
@@ -1,3 +0,0 @@
-export const fork = <T>(checkFunction: (ctx: T) => boolean, ifTrue: (ctx: T) => any, ifFalse: (ctx: T) => any) => (
-  ctx: T
-) => (checkFunction(ctx) ? ifTrue(ctx) : ifFalse(ctx));
