d2421298e2c1428a6f462bbfe38a3656429d9ba1
Sergei Orlov <priestine1.dev@gmail.com>
2019-06-10 16:58:37 +0300

    refactor: rewrite with functional style

--- src/middleware/exit-if-version-is-not-updated.middleware.ts
+++ src/middleware/exit-if-version-is-not-updated.middleware.ts
@@ -1,14 +1,23 @@
 import { SemanticsCtx } from '../interfaces/semantics-intermediate.interface';
 import { Log } from '../utils/log.util';
 import { Iro } from '@priestine/iro/src';
+import { fork } from '../utils/fork.util';
+import * as R from 'ramda';
 
-export function exitIfVersionIsNotUpdated({ intermediate }: SemanticsCtx) {
-  if (intermediate.latestVersionTag === intermediate.newVersion) {
-    Log.warning('Evaluated changes do not require version bumping. Terminating.');
-    return process.exit(0);
-  }
+const versionUpdated = R.lift(R.equals)(
+  R.path(['intermediate', 'newVersion']),
+  R.path(['intermediate', 'latestVersionTag'])
+);
 
+const handleSameVersion = () => {
+  Log.warning('Evaluated changes do not require version bumping. Terminating.');
+  return process.exit(0);
+};
+
+const handleVersionUpdate = ({ intermediate }: SemanticsCtx) => {
   Log.success(`New version candidate: ${Iro.green(`${intermediate.newVersion}`)}`);
 
   return intermediate;
-}
+};
+
+export const exitIfVersionIsNotUpdated = fork(versionUpdated, handleSameVersion, handleVersionUpdate);
